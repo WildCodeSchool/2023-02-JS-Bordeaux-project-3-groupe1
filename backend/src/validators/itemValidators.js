e347c0cd28c749b81fdf1402613ec6fd23396857
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-05-31 14:14:45 +0200

    préparation projet 3 Ligne Bleu

---
+++ backend/src/validators/itemValidators.js
@@ -0,0 +1,1 @@
+const { body, validatedResult } = require("express-validator");
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from 'react-dom/client'
+import "./App.scss";
+import Root from './routes/Root';
+import HomePage from './pages/HomePage';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      }
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  )
