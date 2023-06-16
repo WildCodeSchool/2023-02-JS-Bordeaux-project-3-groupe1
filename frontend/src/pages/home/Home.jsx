0035a6816213d53873dfc8f2724ab97c2545e428
Laetitia <laetitia.mdlb@gmail.com>
2023-06-16 16:54:27 +0200

    modif homepage

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,54 +1,48 @@
 import { Link } from "react-router-dom";
 import ImageAccueil from "../../assets/pictures/femmebureau.svg";
-import Support from "../../assets/pictures/Rectangle_welcome_120.svg";
+import Support from "../../assets/pictures/Rectangle_bleu.png";
 import Presenter from "../../assets/pictures/presentatrice.png";
-import Footer from "../../components/footer/Footer";
 import "../../assets/styles/variables.scss";
 
 function Home() {
   return (
-    <>
-      <main className="mainHome">
-        <div className="header">
-          <div className="bloc_women">
-            <img className="desk_women" src={ImageAccueil} alt="femme_bureau" />
-            <img
-              className="decorative_rectangle"
-              src={Support}
-              alt="rectangle_décoratif"
-            />
-          </div>
-          <h1 className="objectif">
-            Rendre visible les invisibles du numérique
-          </h1>
+    <main className="mainHome">
+      <div className="header">
+        <div className="bloc_women">
+          <img className="desk_women" src={ImageAccueil} alt="femme_bureau" />
+          <img
+            className="decorative_rectangle"
+            src={Support}
+            alt="rectangle_décoratif"
+          />
         </div>
-        <div>
-          <h2 className="instructions">
-            Suivez nos formations afin de progresser à votre vitesse en
-            utilisant notre plateforme
+        <h1 className="objectif">Rendre visible les invisibles du numérique</h1>
+      </div>
+      <div>
+        <h2 className="instructions">
+          Suivez nos formations afin de progresser à votre vitesse en utilisant
+          notre plateforme
+        </h2>
+      </div>
+      <div className="button_debuter_formation">
+        <Link to="/LevelUser">
+          <button className="button_Debuter_Formation" type="button">
+            Débuter ma formation
+          </button>
+        </Link>
+      </div>
+      <div className="presenter">
+        <img className="presenter_img" src={Presenter} alt="présentatrice" />
+        <div className="bloc_presenter">
+          <h2 className="text_presenter">
+            Suivre notre tutoriel de prise en main de notre plateforme
+            <Link to="../platformTutorial" className="word_ici">
+              ici
+            </Link>
           </h2>
         </div>
-        <div className="button_debuter_formation">
-          <Link to="/LevelUser">
-            <button className="button_Debuter_Formation" type="button">
-              Débuter ma formation
-            </button>
-          </Link>
-        </div>
-        <div className="presenter">
-          <img className="presenter_img" src={Presenter} alt="présentatrice" />
-          <div className="bloc_presenter">
-            <h2 className="text_presenter">
-              Suivre notre tutoriel de prise en main de notre plateforme
-              <a href="../platformTutorial" className="word_ici">
-                ici
-              </a>
-            </h2>
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </>
+      </div>
+    </main>
   );
 }
 export default Home;
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar/NavBar";
+import Footer from "../components/footer/Footer";
 
 function Root() {
   return (
     <div>
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
