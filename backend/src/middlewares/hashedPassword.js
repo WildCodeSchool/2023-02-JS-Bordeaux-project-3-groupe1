7dcca1539db27d205303f97b2bbb4e204fb8ae26
Téo cathelot <t.cathelot.pro@gmail.com>
2023-06-21 15:59:09 +0200

    register hashedPassword and validator

---
+++ backend/src/middlewares/hashedPassword.js
@@ -0,0 +1,58 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const argon2 = require("argon2");
+
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 16,
+  timeCost: 5,
+  parallelism: 1,
+};
+
+const hashPassword = (req, res, next) => {
+  argon2
+    .hash(req.body.password, hashingOptions)
+    .then((hashedPassword) => {
+      console.info(hashedPassword);
+
+      req.body.hashedPassword = hashedPassword;
+      delete req.body.password;
+
+      next();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  hashPassword,
+};
+
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// // const argon2 = require("argon2");
+
+// const hashingOptions = {
+//   type: argon2.argon2id,
+//   memoryCost: 2 ** 16,
+//   timeCost: 5,
+//   parallelism: 1,
+// };
+
+// const hashPassword = (req, res, next) => {
+//   argon2
+//     .hash(req.body.password, hashingOptions)
+//     .then((hashedPassword) => {
+//       req.body.hashedPassword = hashedPassword;
+//       delete req.body.password;
+//       next();
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.sendStatus(500);
+//     });
+// };
+
+// module.exports = {
+//   hashPassword,
+// };
