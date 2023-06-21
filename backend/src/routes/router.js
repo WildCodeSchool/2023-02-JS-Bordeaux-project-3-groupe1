889a1f4f1d87f182cbf52740b604be0fe31b6345
Gaby1988 <110187007+Gaby1988@users.noreply.github.com>
2023-06-21 13:34:22 +0200

    Update router.js

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -2,15 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const imageRouter = require("./imageRouter");
-const iconRouter = require("./iconRouter");
 const tutorialByIconRouter = require("./tutorialByIconRouter");
 const tutorialRouter = require("./tutorialRouter");
 const formationRouter = require("./formationRouter");
 const tutorialTagRouter = require("./tutorialTagRouter");
 
-router.use("/uploads", imageRouter);
-router.use("/icons", iconRouter);
 router.use("/tutorialbyicon", tutorialByIconRouter);
 router.use("/tutorials", tutorialRouter);
 router.use("/formations", formationRouter);
