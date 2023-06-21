fa88f9f9eda2de68842ad02e97e9fed51c02a4e1
Téo cathelot <t.cathelot.pro@gmail.com>
2023-06-21 16:28:05 +0200

    resolve argon2 issue

--- backend/src/routes/registerRouter.js
+++ backend/src/routes/registerRouter.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 const registerController = require("../controllers/registerController");
+const { hashPassword } = require("../middlewares/hashedPassword");
+const { validateRegister } = require("../validators/registerValidator");
 
-router.post("/", registerController.postUsers);
+router.post("/", hashPassword, validateRegister, registerController.postUsers);
 router.get("/", registerController.getUsers);
 
 module.exports = router;
--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const tutorialRouter = require("./tutorialRouter");
 const formationRouter = require("./formationRouter");
 const tutorialTagRouter = require("./tutorialTagRouter");
+const registerRouter = require("./registerRouter");
 
 router.use("/tutorials", tutorialRouter);
 router.use("/formations", formationRouter);
 router.use("/tutorialsTags", tutorialTagRouter);
+router.use("/register", registerRouter);
 
 module.exports = router;
