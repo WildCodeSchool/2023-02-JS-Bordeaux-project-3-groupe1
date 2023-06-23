dc7e98c770976c6c715bb0fd48112fb4c8f619c5
Gaby1988 <110187007+Gaby1988@users.noreply.github.com>
2023-06-23 10:48:03 +0200

    Update main.jsx
    rajout du /:id ligne 36

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element: <Formation />,
       },
       {
-        path: "/formations/tutorials",
+        path: "/formations/tutorials/:id",
         element: <TutorialChoice />,
       },
       {
