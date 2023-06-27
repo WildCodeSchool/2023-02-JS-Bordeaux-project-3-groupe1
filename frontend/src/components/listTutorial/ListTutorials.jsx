7e8e9f7104fa91aad86af0a68c47d6a21889ee2b
elea641 <eleavoliotis64@gmail.com>
2023-06-27 16:36:24 +0200

    search tutorial by tags

--- frontend/src/components/listTutorial/ListTutorials.jsx
+++ frontend/src/components/listTutorial/ListTutorials.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { fetcher } from "../../services/api";
 import student from "../../assets/student.png";
 import starGrey from "../../assets/starGrey.png";
 
-function ListTutorials() {
+function ListTutorials({ search }) {
   const [tutorials, setTutorials] = useState([]);
-  const [tagsList, setTagsList] = useState([]);
 
   useEffect(() => {
     fetcher("tutorials")
@@ -17,23 +17,17 @@ function ListTutorials() {
       });
   }, []);
 
-  useEffect(() => {
-    const newTagsList = tutorials.map((item) => {
-      const tagsArray = item.nameTag.split(",").map((value) => value.trim());
-      return {
-        tutorialId: item.id,
-        tags: tagsArray,
-      };
-    });
-    setTagsList(newTagsList);
-  }, [tutorials]);
-
-  console.warn(tagsList);
+  const filteredTutorials = tutorials.filter((item) => {
+    const tagsArray = item.nameTag.split(",").map((value) => value.trim());
+    return tagsArray.some((tag) =>
+      tag.toLowerCase().includes(search.toLowerCase())
+    );
+  });
 
   return (
     <div className="container-listTutorials">
       <ul className="container-listTutorials-preview">
-        {tutorials.map((item) => (
+        {filteredTutorials.map((item) => (
           <li key={item.id}>
             {item?.level === 1 ? (
               <div className="container-icon">
@@ -64,5 +58,9 @@ function ListTutorials() {
     </div>
   );
 }
+
+ListTutorials.propTypes = {
+  search: PropTypes.string.isRequired,
+};
 
 export default ListTutorials;
--- frontend/src/components/search/SearchTurotial.jsx
+++ frontend/src/components/search/SearchTurotial.jsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from "react";
 import ListTutorials from "../listTutorial/ListTutorials";
 
-function SearchTutorial({ tutorials }) {
+function SearchTutorial() {
   const [search, setSearch] = useState("");
-  const [tagsList, setTagsList] = useState("");
 
   const handleInputChange = (e) => {
     setSearch(e.target.value);
   };
 
-  useEffect(() => {
-    const nameTagsArray = tutorials.map((value) => value.nameTag);
-    const newArrayNameTags = nameTagsArray
-      .toString()
-      .split(",")
-      .map((value) => value.trim());
-    setTagsList(newArrayNameTags);
-  }, [tutorials]);
-
-  console.warn(tagsList);
   return (
     <div className="container-searchTutorial">
       <label htmlFor="search">Rechercher un tutoriel</label>
@@ -33,27 +21,9 @@ function SearchTutorial({ tutorials }) {
           placeholder="Recherche par tags ..."
         />
       </div>
-      <ListTutorials />
+      <ListTutorials search={search} />
     </div>
   );
 }
-
-SearchTutorial.propTypes = {
-  tutorials: PropTypes.arrayOf(
-    PropTypes.shape({
-      formation_id: PropTypes.number,
-      id: PropTypes.number,
-      level: PropTypes.number,
-      name: PropTypes.string,
-      nameTag: PropTypes.string,
-      objectif: PropTypes.string,
-      pictureTuto: PropTypes.string,
-      quizz_id: PropTypes.number,
-      tag_id: PropTypes.number,
-      tutorial_id: PropTypes.number,
-      urlVideo: PropTypes.string,
-    })
-  ).isRequired,
-};
 
 export default SearchTutorial;
