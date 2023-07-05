423369e40aeb3490527d710ccb7ded77ea2e723c
Téo cathelot <Votre adresse e-mail>
2023-07-05 10:19:06 +0200

    register update

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import invisible from "../../assets/invisible.png";
 import visible from "../../assets/visible.png";
