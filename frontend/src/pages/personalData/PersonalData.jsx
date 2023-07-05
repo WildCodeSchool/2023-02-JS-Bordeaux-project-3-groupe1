adb7cb2e439cf7365436b4988d74b2c8dc2d3157
Laetitia <laetitia.mdlb@gmail.com>
2023-07-05 16:02:28 +0200

    create page PersonalData

---
+++ frontend/src/pages/personalData/PersonalData.jsx
@@ -0,0 +1,157 @@
+function PersonalData() {
+  return (
+    <div className="containePersonalData">
+      <h3 className="titlePersonalData">
+        Politique de confidentialité et de protection des Données Personnelles
+        du groupe Ligne Bleue
+      </h3>
+      <h4 className="subtitlePersonalData">Gérer vos données personnelles</h4>
+      <h5 className="paragraphTitlePersonalData">
+        Politique de protection des données personnelles
+      </h5>
+      <p className="pPersonalData">
+        Le groupe Ligne Bleue place la protection des données à caractère
+        personnel au cœur de ses missions et des services qui vous sont
+        proposés.
+      </p>
+      <p className="pPersonalData">
+        La présente Politique énonce les principes et lignes directrices pour la
+        protection de vos Données Personnelles et a pour objectif de vous
+        informer sur :
+        <ul>
+          <li className="pPersonalData">
+            Les Données Personnelles que le groupe La Poste collecte et les
+            raisons de cette collecte,
+          </li>
+          <li className="pPersonalData">
+            {" "}
+            La façon dont sont utilisées ces Données Personnelles,
+          </li>
+          <li className="pPersonalData">
+            {" "}
+            ainsi que vos droits sur vos Données Personnelles.
+          </li>
+        </ul>
+      </p>
+      <h5 className="paragraphTitlePersonalData">
+        I - Comment le groupe Ligne Bleue prend-t-il en compte la protection des
+        données personnelles ?
+      </h5>
+      <p className="pPersonalData">
+        Le groupe La Poste s’engage à prendre en compte la protection de vos
+        Données Personnelles et de votre vie privée dès la conception des
+        nouveaux produits ou services qui vous sont proposés.
+      </p>
+      <p className="pPersonalData">
+        Pour assurer la sécurité et garantir le respect et le bon exercice de
+        vos droits, les mesures permettant d’assurer la protection de vos
+        données personnelles sont mises en œuvre.
+      </p>
+      <h5 className="paragraphTitlePersonalData">
+        II - Comment le groupe Ligne Bleue collecte-t-il vos données
+        personnelles ?
+      </h5>
+      <p className="pPersonalData">
+        Le groupe Ligne Bleue s’engage à ne collecter que les données
+        strictement nécessaires à la réalisation directe ou indirecte des
+        services souscrits lorsqu’ils nécessitent de traiter les données
+        personnelles des clients.
+      </p>
+      <p className="pPersonalData">
+        Dans le cas où des données facultatives vous seraient demandées, le
+        groupe Ligne Bleue vous informera clairement sur les Données
+        Personnelles indispensables à la réalisation du service souscrit.
+      </p>
+      <p className="pPersonalData">
+        Les Données Personnelles sont collectées directement auprès de vous et
+        ne sont utilisées que pour les usages qui ont été portés à votre
+        connaissance.
+      </p>
+      <p className="pPersonalData">
+        Les données personnelles sont utilisées pour vous proposer d’autres
+        services, uniquement si vous avez accepté de recevoir des communications
+        commerciales.
+      </p>
+      <p className="pPersonalData">
+        Certaines données traitées par La Poste sont collectées de manière
+        indirecte à partir des sources suivantes :
+        <ul>
+          <li className="pPersonalData">
+            {" "}
+            Soit des clients, précisant des informations sur des souscripteurs,
+            des bénéficiaires, des ayants-droits, des contacts, des
+            destinataires. Ces données sont nécessaires à l’exécution des
+            services souscrits,
+          </li>
+          <li className="pPersonalData">
+            Soit des tiers, tels que des organismes de prévention de la fraude,
+            des fournisseurs de données, des organisations (Organisation
+            Mondiale des Douanes, …) et des membres de l’Union Postale
+            Universelle. En cas de collecte indirecte, La Poste s’engage à
+            informer les personnes dans le respect des conditions posées à
+            l’article 14 du RGPD.
+          </li>
+        </ul>
+      </p>
+      <p className="pPersonalData">
+        Certains services peuvent être utilisés par des mineurs. Dans ce cas,
+        les mineurs doivent obtenir le consentement de leurs parents ou
+        représentants légaux.
+      </p>
+      <h5 className="paragraphTitlePersonalData">
+        III - Quelles sont les finalités des traitements effectués par le groupe
+        Ligne Bleue et la durée de validité de conservation des données
+        personnelles ?
+      </h5>
+      <p className="pPersonalData">
+        D’une manière générale, les finalités, la durée de conservation et la
+        base légale diffèrent selon les services et produits concernés.
+      </p>
+      <h5 className="paragraphTitlePersonalData">
+        IV - Vos données personnelles sont-elles protégées ?
+      </h5>
+      <p className="pPersonalData">
+        Le Groupe Ligne Bleue s’engage à prendre toutes mesures afin d’assurer
+        la sécurité et la confidentialité des Données Personnelles. En
+        particulier, le Groupe Ligne Bleue met en œuvre toutes les mesures
+        techniques et organisationnelles propres à garantir la sécurité et la
+        confidentialité des Données Personnelles collectées et traitées et
+        notamment à empêcher qu’elles soient déformées, endommagées ou
+        communiquées à des tiers non autorisés, en assurant un niveau de
+        sécurité adapté aux risques liées au traitement et à la nature des
+        données personnelles à protéger.
+      </p>
+      <h5 className="paragraphTitlePersonalData">
+        V - Quels sont vos droits sur vos données personnelles et comment les
+        exercer ?
+      </h5>
+      <p className="pPersonalData">
+        Vous avez le droit d’accéder aux données personnelles que nous détenons
+        à votre sujet ;
+      </p>
+      <p className="pPersonalData">
+        Cela inclut le droit de nous demander des informations complémentaires
+        sur:
+        <ul>
+          <li className="pPersonalData">
+            les catégories de données que nous traitons
+          </li>
+          <li className="pPersonalData">
+            les finalités du traitement des données
+          </li>
+          <li className="pPersonalData">
+            les destinataires et les catégories de destinataires à qui vos
+            données ont été transmises
+          </li>
+          <li className="pPersonalData">
+            {" "}
+            lorsque cela est possible, la durée de conservation de vos données
+            ou lorsque ce n’est pas possible les critères pour déterminer cette
+            durée.
+          </li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+export default PersonalData;
