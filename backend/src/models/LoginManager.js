d12348ba71b8c5673d33f36646836b5b43b6ae63
Elea64 <107116121+Elea641@users.noreply.github.com>
2023-07-06 17:09:36 +0200

    Update LoginManager.js

--- backend/src/models/LoginManager.js
+++ backend/src/models/LoginManager.js
@@ -2,8 +2,6 @@ const database = require("../../database");
 
 const findByEmail = async (login) => {
   const query = "SELECT * FROM users WHERE email = ? ";
-  // const { email, hashedPassword } = login;
-  // const value = [email, hashedPassword];
 
   try {
     const [user] = await database.query(query, login);
