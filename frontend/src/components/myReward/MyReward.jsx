99b496b05c9fb55ac25df962fce32c1e9b3de9d6
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-07-08 18:09:53 +0200

    correction de update des icons en fonction de la formation

--- frontend/src/components/myReward/MyReward.jsx
+++ frontend/src/components/myReward/MyReward.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function MyReward({ icon, iconOpacityLow }) {
-  return <img className={iconOpacityLow} src={icon.iconURL} alt={icon.name} />;
+function MyReward({ icon, tutorialByIcon }) {
+  const isCompleted = tutorialByIcon
+    .filter(
+      (tutorial) => parseInt(tutorial.total_stepsTotal, 10) >= tutorial.NB_tuto
+    )
+    .some((tutorial) => tutorial.iconURL === icon.iconURL);
+
+  return (
+    <img
+      className={isCompleted ? "myRewardIcons" : "myRewardIconsChange"}
+      src={icon.iconURL}
+      alt={icon.name}
+    />
+  );
 }
-export default MyReward;
+
 MyReward.propTypes = {
-  iconOpacityLow: PropTypes.string.isRequired,
   icon: PropTypes.shape({
     iconURL: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-  }),
+  }).isRequired,
+  tutorialByIcon: PropTypes.arrayOf(
+    PropTypes.shape({
+      iconURL: PropTypes.string.isRequired,
+      NB_tuto: PropTypes.number.isRequired,
+      total_stepsTotal: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
-MyReward.defaultProps = {
-  icon: {
-    iconURL: "firebase",
-    name: "texte de l'icone",
-  },
-};
+
+export default MyReward;
