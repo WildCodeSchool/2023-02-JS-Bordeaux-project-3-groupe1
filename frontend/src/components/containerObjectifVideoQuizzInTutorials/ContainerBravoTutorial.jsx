f6cf515fd0270f00d84811793b5ab202904d00cc
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-07-10 15:40:20 +0200

    save perso

---
+++ frontend/src/components/containerObjectifVideoQuizzInTutorials/ContainerBravoTutorial.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContainerBravoTutorial() {
+	return (
+		<div>
+			<p className="pTutoExplication">Explications</p>
+			<div className="container-Explications-bravo">
+				<h1>Bravo</h1>
+			</div>
+		</div>
+	);
+}
+export default ContainerBravoTutorial;
