b6e3d0278a7e9e04a82fc08e8542acaecb74c90e
Gaby1988 <110187007+Gaby1988@users.noreply.github.com>
2023-07-12 20:20:45 +0200

    Update ContainerBravoTutorial.jsx

--- frontend/src/components/containerObjectifVideoQuizzInTutorials/ContainerBravoTutorial.jsx
+++ frontend/src/components/containerObjectifVideoQuizzInTutorials/ContainerBravoTutorial.jsx
@@ -3,21 +3,21 @@ import { PropTypes } from "prop-types";
 import Bravo from "../../assets/pictures/bravo 1.png";
 
 function ContainerBravoTutorial({ dataTutorial }) {
-	return (
-		<div>
-			<p className="pTutoBravo">Bravo</p>
-			<div className="encartBravo">
-				<div className="container-bravo">
-					<div className="container-Bravo-preview">
-						<img className="bravoPicture" src={Bravo} alt="félicitations" />
-						<p className="pBravoPicture">
-							Vous avez terminé le tuto "{dataTutorial}"
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+ return (
+    <div>
+      <p className="pTutoBravo">Bravo</p>
+      <div className="encartBravo">
+        <div className="container-bravo">
+          <div className="container-Bravo-preview">
+            <img className="bravoPicture" src={Bravo} alt="félicitations" />
+            <p className="pBravoPicture">
+              Vous avez terminé le tuto "{dataTutorial}"
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ContainerBravoTutorial.propTypes = {
