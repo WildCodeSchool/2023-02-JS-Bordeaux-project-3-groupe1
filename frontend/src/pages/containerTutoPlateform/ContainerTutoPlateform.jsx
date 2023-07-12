d6697541b4b7ee72e3fd2bdd9208a92b24be0fc6
Laetitia <laetitia.mdlb@gmail.com>
2023-07-12 11:37:13 +0200

    bugFix scss prise en main de la plateforme

--- frontend/src/pages/containerTutoPlateform/ContainerTutoPlateform.jsx
+++ frontend/src/pages/containerTutoPlateform/ContainerTutoPlateform.jsx
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import NameMenuTopContext from "../../contexts/NameMenuTopContext";
 import ButtonTutorial from "../../components/containerObjectifVideoQuizzInTutorials/ButtonTutorial";
 import Pouce from "../../assets/pouce.png";
+import Cible from "../../assets/pictures/cible.png";
+import { IsDesktopContext } from "../../contexts/IsDesktopContext";
 
 function ContainerTutoPlateform() {
   const { setNameMenu } = useContext(NameMenuTopContext);
+  const { isDesktop } = useContext(IsDesktopContext);
   setNameMenu("Prise en main de la plateforme");
   return (
     <div className="container-ObjectifTutorial">
@@ -13,10 +16,19 @@ function ContainerTutoPlateform() {
           <img className="pouce" src={Pouce} alt="pouce" />
           <div className="container-Title-Tuto">
             <h3 className="objectif">Objectif</h3>
+            {isDesktop ? (
+              <div>
+                <img src={Cible} className="ciblePlateform" alt="cible" />
+              </div>
+            ) : (
+              <div />
+            )}
           </div>
         </div>
         <div className="container-ObjectifText">
-          <p>Comprendre au mieux l’utilisation de notre plateforme</p>
+          <p className="textObjectifPlateform">
+            Comprendre au mieux l’utilisation de notre plateforme
+          </p>
         </div>
       </div>
       <div className="line-yellowPlateform" />
