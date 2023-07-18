982eff50a29802162aee7bef4dd5d0fe5b3a7cf9
Elea Voliotis <eleavoliotis@MacBook-Pro-de-Elea.local>
2023-07-18 12:14:33 +0200

    scss

--- frontend/src/pages/admin/UserInfo.jsx
+++ frontend/src/pages/admin/UserInfo.jsx
@@ -17,14 +17,20 @@ function UserInfo() {
   }, []);
   return (
     <div className="container-userInfo">
-      {user?.picture && <img src={user.picture} alt="preview user" />}
-      <h3>Prénom : {user.firstname}</h3>
-      <h3>Nom : {user.lastname}</h3>
-      <h3>Mail : {user.email}</h3>
-      <h3>Ville : {user.city}</h3>
-      <h3>Code Postal : {user.location}</h3>
-      <h3>Genre : {user.gender}</h3>
-      <h3>Level : {user.level}</h3>
+      {user?.picture ? (
+        <img src={user.picture} alt="preview user" />
+      ) : (
+        <div className="container-userInfo-picture" />
+      )}
+      <div className="container-userInfo-infos">
+        <h3>Prénom : {user.firstname}</h3>
+        <h3>Nom : {user.lastname}</h3>
+        <h3>Mail : {user.email}</h3>
+        <h3>Ville : {user.city}</h3>
+        <h3>Code Postal : {user.location}</h3>
+        <h3>Genre : {user.gender}</h3>
+        <h3>Level : {user.level}</h3>
+      </div>
     </div>
   );
 }
